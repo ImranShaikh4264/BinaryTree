c7d142243bc5a42e1a8e3902e0400420842d8d91
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-24 17:38:28 +0530

    Added: Add more element to binary tree and find size of binry tree using size method

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,46 +2,65 @@ package com.bridgelab.BinaryTree;
 
 public class BinaryTree {
 	Node root;
+	static int size = 0;
 
 	// Method to add data in tree
-		public void addElement(int data) {
-			Node node = new Node(data);
-			node.data = data;
-			node.left = null;
-			node.right = null;
+	public void addElement(int data) {
+		Node node = new Node(data);
+		node.data = data;
+		node.left = null;
+		node.right = null;
 
-			if (root == null) {
-				root = node;
-				return;
-			} else {
-				Node parent = root;
-				Node current = root;
-				while (current != null) {
-					parent = current;
-					if (node.data > current.data) {
-						current = current.right;
-					} else {
-						current = current.left;
-					}
-				}
-				if (node.data > parent.data) {
-					parent.right = node;
+		if (root == null) {
+			root = node;
+			return;
+		} 
+		else {
+			Node parent = root;
+			Node current = root;
+			while (current != null) {
+				parent = current;
+				if (node.data > current.data) {
+					current = current.right;
 				} else {
-					parent.left = node;
+					current = current.left;
 				}
 			}
+			if (node.data > parent.data) {
+				parent.right = node;
+			} 
+			else {
+				parent.left = node;
+			}
 		}
+	}
+
+	public static int size(Node node) {
+		
+		if (node.left != null) {
+			size(node.left);
+
+		}
+		size++;
+
+		if (node.right != null) {
+			size(node.right);
+		
+		}
+		return size;
+
+	}
 
 	// Method to display data of tree
-		public void display(Node node) {
-			if (node.left != null) {
-				display(node.left);
-			}
-
-			System.out.print(node.data + " ");
-
-			if (node.right != null) {
-				display(node.right);
-			}
+	public void display(Node node) {
+		if (node.left != null) {
+			display(node.left);
 		}
+
+		System.out.print(node.data + " ");
+
+		if (node.right != null) {
+			display(node.right);
+		}
+	}
 }
--- BinaryTreeMain.java
+++ BinaryTreeMain.java
@@ -8,7 +8,18 @@ public class BinaryTreeMain {
 		tree.addElement(56);
 		tree.addElement(30);
 		tree.addElement(70);
-
+		tree.addElement(22);
+		tree.addElement(40);
+		tree.addElement(11);
+		tree.addElement(3);
+		tree.addElement(16);
+		tree.addElement(35);
+		tree.addElement(60);
+		tree.addElement(93);
+		tree.addElement(63);
+		tree.addElement(67);
+		
+		System.out.println("The number of element "+tree.size(tree.root));
 		System.out.println("\nBinary search tree after insertion of element");
 
 		tree.display(tree.root);
